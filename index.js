d1142776a2c3484f4388866b80d5724270d3dc98
Anonymous2422 <vaibhavgholap4411@gmail.com>
2020-07-06 00:40:33 +0530

    Initial setup

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const connectDebug = require("debug")("app:connect");
 
 app.use(express.json());
 
+app.get("/", (req, res) => {
+	res.send("<h1>DevScript setup</h1>");
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
-  connectDebug(`listening at port ${PORT}`);
+	connectDebug(`listening at port ${PORT}`);
 });
