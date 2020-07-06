1dd03c2655ced316d91defac2a00d29807b6eb0a
divaymohan <divaymohan05@gmail.com>
2020-07-06 15:17:53 +0530

    clean code and working

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const app = express();
 const connectDebug = require("debug")("app:connect");
 app.use(express.json());
 
-
 require("./startup/extraTools")(app);
 require("./startup/routes")(app);
 require("./startup/logging")();
@@ -14,13 +13,4 @@ const PORT = 3001;
 app.listen(PORT, () => {
   connectDebug(`listening at port ${PORT}`);
   winston.info(`listening at port ${PORT}`);
-
-app.get("/", (req, res) => {
-	res.send("<h1>DevScript setup</h1>");
-});
-
-const PORT = 3001;
-app.listen(PORT, () => {
-	connectDebug(`listening at port ${PORT}`);
-
 });
