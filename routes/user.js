81a722a95e2741e3084fc266d3b326f9dad41346
Divay Mohan <divaymohan05@gmail.com>
2020-07-07 11:31:41 +0530

    routes added to the user end point

--- routes/user.js
+++ routes/user.js
@@ -1,20 +1,40 @@
 const express = require("express");
 const { validateUser } = require("../models/user");
-const { getUsers } = require("../database/user");
+const { getUsers, getUser, addUser } = require("../database/user");
 const Router = express.Router();
 
 //get all users handler
 Router.get("/", (req, res) => {
     try {
         const result = await getUsers();
+        if(result.length==0) return res.send(["No Item Found In Database"]);
         return res.send(result);
     } catch (err) {
-        return res.send(["No Item Found In Database"]);
+        return res.status(200).send(err.message);
     }
  
 });
 //get one user by id
-
-Router.get('/:id',(req,res)=>{})
+Router.get('/:id',(req,res)=>{
+    try {
+        const result = await getUser(req.params.id);
+        if(!result) return res.status(401).send(`No user found with id ${req.params.id}`);
+        return res.send(result);
+    } catch (error) {
+        return res.status(200).send(err.message);
+    }
+});
+//add one user
+Router.post('/',(req,res)=>{
+    const {error} = validateUser(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+    try {
+        const result = await addUser(req.body);
+        if(!result) return res.status(200).send(`Server Error Can't Store Your Data Please Try Again.`);
+        return result;
+    } catch (err) {
+        return res.status(200).send(err.message);
+    }
+})
 
 module.exports = Router;
