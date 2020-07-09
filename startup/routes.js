be1ce6c47178028774ba49ed345770e93cecddae
Divay Mohan <divaymohan05@gmail.com>
2020-07-09 19:15:41 +0530

    adding topics end point and all are working now

--- startup/routes.js
+++ startup/routes.js
@@ -3,6 +3,7 @@ const _user = require("../routes/user");
 const _course = require("../routes/course");
 const _contributor = require("../routes/contributor");
 const _video = require("../routes/video");
+const _topic = require("../routes/topic");
 
 module.exports = function (app) {
   app.use("/api/articles", _article);
@@ -10,4 +11,5 @@ module.exports = function (app) {
   app.use("/api/courses", _course);
   app.use("/api/contributors", _contributor);
   app.use("/api/videos", _video);
+  app.use("/api/topics", _topic);
 };
